024357ec48729fec54d135132094975206230dc5
tibbi <tibor@kaputa.sk>
2016-01-26 23:05:09 +0100

    add a sample row to the table layout

--- app/src/main/java/calendar/simplemobiletools/com/MainActivity.java
+++ app/src/main/java/calendar/simplemobiletools/com/MainActivity.java
@@ -2,6 +2,9 @@ package calendar.simplemobiletools.com;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TableLayout tableHolder = (TableLayout) findViewById(R.id.table_holder);
+        LayoutInflater inflater = getLayoutInflater();
+        TableRow row = (TableRow) inflater.inflate(R.layout.table_row, tableHolder, false);
+        tableHolder.addView(row);
     }
 }
