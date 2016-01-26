6e4d4a4f7ed4cf0238902a72c7fe8fa6f05e50b4
tibbi <tibor@kaputa.sk>
2016-01-26 23:19:32 +0100

    fill the screen with the table rows and cols

--- app/src/main/java/calendar/simplemobiletools/com/MainActivity.java
+++ app/src/main/java/calendar/simplemobiletools/com/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
         TableLayout tableHolder = (TableLayout) findViewById(R.id.table_holder);
         LayoutInflater inflater = getLayoutInflater();
-        TableRow row = (TableRow) inflater.inflate(R.layout.table_row, tableHolder, false);
-        tableHolder.addView(row);
+
+        for (int i = 0; i < 5; i++) {
+            final TableRow row = (TableRow) inflater.inflate(R.layout.table_row, tableHolder, false);
+            for (int j = 1; j < 8; j++) {
+                final TextView day = (TextView) inflater.inflate(R.layout.table_day, row, false);
+                day.setText("" + (i * 7 + j));
+                row.addView(day);
+            }
+
+            tableHolder.addView(row);
+        }
     }
 }
