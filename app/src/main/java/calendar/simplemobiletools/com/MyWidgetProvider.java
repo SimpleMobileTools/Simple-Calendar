c4e0212c1c4aef571c8775b4b4c1450047937844
tibbi <tibor@kaputa.sk>
2016-01-30 19:01:48 +0100

    add the widget base

---
+++ app/src/main/java/calendar/simplemobiletools/com/MyWidgetProvider.java
@@ -0,0 +1,13 @@
+package calendar.simplemobiletools.com;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class MyWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+}
