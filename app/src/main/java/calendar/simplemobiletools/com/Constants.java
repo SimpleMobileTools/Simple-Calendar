927618b90769a706b316e2ae37d08ad530a89c64
tibbi <tibor@kaputa.sk>
2016-01-31 20:03:34 +0100

    change the high alpha to .9f

--- app/src/main/java/calendar/simplemobiletools/com/Constants.java
+++ app/src/main/java/calendar/simplemobiletools/com/Constants.java
@@ -3,7 +3,7 @@ package calendar.simplemobiletools.com;
 public class Constants {
     public static final String DATE = "date";
     public static final float LOW_ALPHA = .2f;
-    public static final float HIGH_ALPHA = .8f;
+    public static final float HIGH_ALPHA = .9f;
 
     public static final String PREFS = "prefs";
     public static final String WIDGET_BG_COLOR = "widget_bg_color";
