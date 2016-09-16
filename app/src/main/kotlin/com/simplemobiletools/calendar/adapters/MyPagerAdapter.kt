5ac4a95531339fd223af6d85358e40c39790102a
tibbi <tibor@kaputa.sk>
2016-09-16 22:43:07 +0200

    convert MyPagerAdapter to kotlin

---
+++ app/src/main/kotlin/com/simplemobiletools/calendar/adapters/MyPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.simplemobiletools.calendar.adapters
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+import com.simplemobiletools.calendar.Constants
+import com.simplemobiletools.calendar.fragments.MonthFragment
+
+class MyPagerAdapter(fm: FragmentManager, private val mCodes: List<String>, private val mListener: MonthFragment.NavigationListener) : FragmentStatePagerAdapter(fm) {
+
+    override fun getCount(): Int {
+        return mCodes.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        val bundle = Bundle()
+        val code = mCodes[position]
+        bundle.putString(Constants.DAY_CODE, code)
+
+        val fragment = MonthFragment()
+        fragment.arguments = bundle
+        fragment.setListener(mListener)
+        return fragment
+    }
+}
