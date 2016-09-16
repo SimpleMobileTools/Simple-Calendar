228ab622e5223c72ab9e2abeb52e64b925f5e169
tibbi <tibor@kaputa.sk>
2016-09-16 21:32:02 +0200

    convert MainActivity to kotlin

---
+++ app/src/main/kotlin/com/simplemobiletools/calendar/activities/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.simplemobiletools.calendar.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import butterknife.OnClick
+import com.simplemobiletools.calendar.Config
+import com.simplemobiletools.calendar.Constants
+import com.simplemobiletools.calendar.Formatter
+import com.simplemobiletools.calendar.R
+import com.simplemobiletools.calendar.adapters.MyPagerAdapter
+import com.simplemobiletools.calendar.fragments.MonthFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import java.util.*
+
+class MainActivity : SimpleActivity(), MonthFragment.NavigationListener {
+    private val PREFILLED_MONTHS = 73
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val today = DateTime().toString(Formatter.DAYCODE_PATTERN)
+        fillViewPager(today)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Config.newInstance(applicationContext).isFirstRun = false
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> {
+                startActivity(Intent(applicationContext, SettingsActivity::class.java))
+                return true
+            }
+            R.id.about -> {
+                startActivity(Intent(applicationContext, AboutActivity::class.java))
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    @OnClick(R.id.calendar_fab)
+    fun fabClicked(view: View) {
+        val intent = Intent(applicationContext, EventActivity::class.java)
+        val tomorrowCode = Formatter.getDayCodeFromDateTime(DateTime(DateTimeZone.getDefault()).plusDays(1))
+        intent.putExtra(Constants.DAY_CODE, tomorrowCode)
+        startActivity(intent)
+    }
+
+    private fun fillViewPager(targetMonth: String) {
+        val codes = getMonths(targetMonth)
+        val adapter = MyPagerAdapter(supportFragmentManager, codes, this)
+        view_pager.adapter = adapter
+        view_pager.currentItem = codes.size / 2
+    }
+
+    private fun getMonths(code: String): List<String> {
+        val months = ArrayList<String>(PREFILLED_MONTHS)
+        val today = Formatter.getDateTimeFromCode(code)
+        for (i in -PREFILLED_MONTHS / 2..PREFILLED_MONTHS / 2) {
+            months.add(Formatter.getDayCodeFromDateTime(today.plusMonths(i)))
+        }
+
+        return months
+    }
+
+    override fun goLeft() {
+        view_pager.currentItem = view_pager.currentItem - 1
+    }
+
+    override fun goRight() {
+        view_pager.currentItem = view_pager.currentItem + 1
+    }
+
+    override fun goToDateTime(dateTime: DateTime) {
+        fillViewPager(Formatter.getDayCodeFromDateTime(dateTime))
+    }
+}
