841f084c8bc6c03c75f666c14c6576b3e8945866
tibbi <tibor@kaputa.sk>
2016-12-27 22:23:55 +0100

    removing and updating a couple resources

--- app/src/main/kotlin/com/simplemobiletools/calendar/extensions/Cursor.kt
+++
@@ -1,7 +0,0 @@
-package com.simplemobiletools.calendar.extensions
-
-import android.database.Cursor
-
-fun Cursor.getStringValue(key: String) = getString(getColumnIndex(key))
-
-fun Cursor.getIntValue(key: String) = getInt(getColumnIndex(key))
--- app/src/main/kotlin/com/simplemobiletools/calendar/extensions/EditText.kt
+++
@@ -1,5 +0,0 @@
-package com.simplemobiletools.calendar.extensions
-
-import android.widget.EditText
-
-val EditText.value: String get() = text.toString().trim()
