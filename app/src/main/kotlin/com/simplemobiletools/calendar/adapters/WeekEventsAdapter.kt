6ad9819ef34dd19dc36f600ea1d080e341fb0924
tibbi <tibor@kaputa.sk>
2017-01-22 11:19:09 +0100

    mark the pressed empty grid in weekly view on click

--- app/src/main/kotlin/com/simplemobiletools/calendar/adapters/WeekEventsAdapter.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/adapters/WeekEventsAdapter.kt
@@ -1,23 +1,41 @@
 package com.simplemobiletools.calendar.adapters
 
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import com.simplemobiletools.calendar.R
+import com.simplemobiletools.calendar.extensions.config
 
 class WeekEventsAdapter(val context: Context) : BaseAdapter() {
     private val mInflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    private val defaultBackground = context.config.backgroundColor
+    private val coloredBackground = context.config.primaryColor
+    private var selectedGrid: ImageView? = null
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         var view = convertView
 
         if (view == null) {
-            view = mInflater.inflate(R.layout.week_grid_item, parent, false)
+            view = mInflater.inflate(R.layout.week_grid_item, parent, false) as ImageView
+            view.background = ColorDrawable(defaultBackground)
         }
 
-        return view!!
+        view.setOnClickListener {
+            selectedGrid?.background = ColorDrawable(defaultBackground)
+            if (selectedGrid == view) {
+                selectedGrid = null
+            } else {
+                view!!.background = ColorDrawable(coloredBackground)
+                (view as ImageView).setImageResource(R.drawable.ic_plus)
+                selectedGrid = view as ImageView
+            }
+        }
+
+        return view
     }
 
     override fun getItem(position: Int) = null
