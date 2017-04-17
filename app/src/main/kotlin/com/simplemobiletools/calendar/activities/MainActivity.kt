0a987e5ff1ab720a0d3be98796de38908d3259b2
tibbi <tibor@kaputa.sk>
2017-04-17 17:38:17 +0200

    update version to 2.3.2

--- app/src/main/kotlin/com/simplemobiletools/calendar/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/activities/MainActivity.kt
@@ -521,6 +521,7 @@ class MainActivity : SimpleActivity(), NavigationListener {
             add(Release(60, R.string.release_60))
             add(Release(62, R.string.release_62))
             add(Release(67, R.string.release_67))
+            add(Release(69, R.string.release_69))
             checkWhatsNew(this, BuildConfig.VERSION_CODE)
         }
     }
