e562b268ba734bd62a0354951cbb5cb72f4b3495
tibbi <tibor@kaputa.sk>
2017-10-25 19:47:59 +0200

    handle all exceptions at upgrading the db to version 13

--- app/src/main/kotlin/com/simplemobiletools/calendar/helpers/DBHelper.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/helpers/DBHelper.kt
@@ -147,7 +147,7 @@ class DBHelper private constructor(val context: Context) : SQLiteOpenHelper(cont
         if (oldVersion < 13) {
             try {
                 createExceptionsTable(db)
-            } catch (ignored: SQLiteException) {
+            } catch (ignored: Exception) {
                 db.execSQL("ALTER TABLE $EXCEPTIONS_TABLE_NAME ADD COLUMN $COL_CHILD_EVENT_ID INTEGER NOT NULL DEFAULT 0")
             }
         }
