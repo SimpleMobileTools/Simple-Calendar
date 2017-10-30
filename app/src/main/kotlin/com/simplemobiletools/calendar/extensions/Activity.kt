120c844405d7a03785a89d3d3e14a1987908ccb4
tibbi <tibor@kaputa.sk>
2017-10-30 18:09:50 +0100

    use Commons for sharing events

--- app/src/main/kotlin/com/simplemobiletools/calendar/extensions/Activity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/extensions/Activity.kt
@@ -1,11 +1,12 @@
 package com.simplemobiletools.calendar.extensions
 
 import android.app.Activity
-import android.content.Intent
-import android.support.v4.content.FileProvider
+import com.simplemobiletools.calendar.BuildConfig
 import com.simplemobiletools.calendar.R
 import com.simplemobiletools.calendar.activities.SimpleActivity
 import com.simplemobiletools.calendar.helpers.IcsExporter
+import com.simplemobiletools.commons.extensions.getFilePublicUri
+import com.simplemobiletools.commons.extensions.shareUri
 import com.simplemobiletools.commons.extensions.toast
 import java.io.File
 
@@ -19,21 +20,8 @@ fun SimpleActivity.shareEvents(ids: List<Int>) {
     val events = dbHelper.getEventsWithIds(ids)
     IcsExporter().exportEvents(this, file, events) {
         if (it == IcsExporter.ExportResult.EXPORT_OK) {
-            val uri = FileProvider.getUriForFile(this, "com.simplemobiletools.calendar.fileprovider", file)
-            val shareTitle = resources.getString(R.string.share_via)
-            Intent().apply {
-                action = Intent.ACTION_SEND
-                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                setDataAndType(uri, contentResolver.getType(uri))
-                putExtra(Intent.EXTRA_STREAM, uri)
-                type = "text/calendar"
-
-                if (resolveActivity(packageManager) != null) {
-                    startActivity(Intent.createChooser(this, shareTitle))
-                } else {
-                    toast(R.string.no_app_for_ics)
-                }
-            }
+            val uri = getFilePublicUri(file, BuildConfig.APPLICATION_ID)
+            shareUri(uri, BuildConfig.APPLICATION_ID)
         }
     }
 }
