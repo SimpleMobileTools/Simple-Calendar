33d9f02cc7c96d230232444237b93b3b84281495
tibbi <tibor@kaputa.sk>
2017-11-13 12:02:53 +0100

    add a short wakelock at creating the reminder

--- app/src/main/kotlin/com/simplemobiletools/calendar/receivers/NotificationReceiver.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/receivers/NotificationReceiver.kt
@@ -3,6 +3,7 @@ package com.simplemobiletools.calendar.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.PowerManager
 import com.simplemobiletools.calendar.extensions.dbHelper
 import com.simplemobiletools.calendar.extensions.notifyEvent
 import com.simplemobiletools.calendar.extensions.scheduleAllEvents
@@ -12,6 +13,10 @@ import com.simplemobiletools.calendar.helpers.Formatter
 
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+        val wakelock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Simple Calendar")
+        wakelock.acquire(5000)
+
         context.updateListWidget()
         val id = intent.getIntExtra(EVENT_ID, -1)
         if (id == -1)
