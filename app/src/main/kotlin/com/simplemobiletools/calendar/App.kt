9a6ee91180af608578ccd58ee695930cb75064b5
tibbi <tibor@kaputa.sk>
2017-12-01 20:32:54 +0100

    initialize Stetho only in debug build

--- app/src/main/kotlin/com/simplemobiletools/calendar/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/App.kt
@@ -2,21 +2,20 @@ package com.simplemobiletools.calendar
 
 import android.support.multidex.MultiDexApplication
 import com.facebook.stetho.Stetho
-import com.simplemobiletools.calendar.BuildConfig.USE_LEAK_CANARY
 import com.simplemobiletools.commons.extensions.checkUseEnglish
 import com.squareup.leakcanary.LeakCanary
 
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
-        if (USE_LEAK_CANARY) {
+        if (BuildConfig.DEBUG) {
             if (LeakCanary.isInAnalyzerProcess(this)) {
                 return
             }
             LeakCanary.install(this)
+            Stetho.initializeWithDefaults(this)
         }
 
         checkUseEnglish()
-        Stetho.initializeWithDefaults(this)
     }
 }
