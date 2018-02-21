cdccf0e2c84362bf4bd7f4bff7e2301b8d67be6a
tibbi <tibor@kaputa.sk>
2018-02-21 14:49:57 +0100

    update commons to 3.12.15

--- app/src/main/kotlin/com/simplemobiletools/calendar/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/activities/MainActivity.kt
@@ -666,7 +666,7 @@ class MainActivity : SimpleActivity(), RefreshRecyclerViewListener {
     }
 
     private fun launchAbout() {
-        val faqItems = arrayOf(
+        val faqItems = arrayListOf(
                 FAQItem(R.string.faq_1_title_commons, R.string.faq_1_text_commons),
                 FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
                 FAQItem(R.string.faq_4_title_commons, R.string.faq_4_text_commons),
