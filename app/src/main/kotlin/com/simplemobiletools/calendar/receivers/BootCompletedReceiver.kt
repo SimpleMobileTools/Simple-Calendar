65e2df089ab76fad4fbd819642aa86bee8749514
tibbi <tibor@kaputa.sk>
2018-09-22 15:54:03 +0200

    remove the db reference from bootReceiver

--- app/src/main/kotlin/com/simplemobiletools/calendar/receivers/BootCompletedReceiver.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/receivers/BootCompletedReceiver.kt
@@ -3,7 +3,6 @@ package com.simplemobiletools.calendar.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.simplemobiletools.calendar.extensions.dbHelper
 import com.simplemobiletools.calendar.extensions.notifyRunningEvents
 import com.simplemobiletools.calendar.extensions.recheckCalDAVCalendars
 import com.simplemobiletools.calendar.extensions.scheduleAllEvents
@@ -11,9 +10,6 @@ import com.simplemobiletools.calendar.extensions.scheduleAllEvents
 class BootCompletedReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        // try just getting a reference to the db so it updates in time
-        context.dbHelper
-
         Thread {
             context.apply {
                 scheduleAllEvents()
