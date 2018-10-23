8c7cb93191afc52d046bb54e45665aaafdba28b6
tibbi <tibor@kaputa.sk>
2018-10-23 23:21:09 +0200

    fix #651, remove the leading zero at Event List widget title

--- app/src/main/kotlin/com/simplemobiletools/calendar/helpers/Formatter.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/helpers/Formatter.kt
@@ -15,7 +15,7 @@ object Formatter {
     const val TIME_PATTERN = "HHmmss"
     private const val DAY_PATTERN = "d"
     private const val DAY_OF_WEEK_PATTERN = "EEE"
-    private const val LONGEST_PATTERN = "MMMM dd YYYY (EEEE)"
+    private const val LONGEST_PATTERN = "MMMM d YYYY (EEEE)"
     private const val PATTERN_TIME_12 = "hh:mm a"
     private const val PATTERN_TIME_24 = "HH:mm"
 
