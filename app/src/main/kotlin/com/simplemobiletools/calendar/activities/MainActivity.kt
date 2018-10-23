44da1202cc773f72aa87e8cf7113f02a40bbb57d
AzisuAzusa <azis.abdul.bachar@gmail.com>
2018-10-23 19:01:52 +0700

    #664 disable pull to refresh when user not synced

--- app/src/main/kotlin/com/simplemobiletools/calendar/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/activities/MainActivity.kt
@@ -90,6 +90,7 @@ class MainActivity : SimpleActivity(), RefreshRecyclerViewListener {
 
         if (config.caldavSync) {
             refreshCalDAVCalendars(false)
+            swipe_refresh_layout.setOnRefreshListener { refreshCalDAVCalendars(false) }
         }
 
         if (!checkViewIntents()) {
@@ -129,6 +130,10 @@ class MainActivity : SimpleActivity(), RefreshRecyclerViewListener {
         search_placeholder_2.setTextColor(config.textColor)
         calendar_fab.setColors(config.textColor, getAdjustedPrimaryColor(), config.backgroundColor)
         search_holder.background = ColorDrawable(config.backgroundColor)
+
+        swipe_refresh_layout.run {
+            isEnabled = config.caldavSync
+        }
     }
 
     override fun onPause() {
@@ -368,6 +373,7 @@ class MainActivity : SimpleActivity(), RefreshRecyclerViewListener {
                         if (showCalDAVRefreshToast) {
                             toast(R.string.refreshing_complete)
                         }
+                        swipe_refresh_layout.isRefreshing = false
                     }
                 }, CALDAV_SYNC_DELAY)
             }
