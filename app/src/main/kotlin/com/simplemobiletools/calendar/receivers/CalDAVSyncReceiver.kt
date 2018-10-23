3114e0bb6e2d3437da08051e0a7c835b5725fb79
tibbi <tibor@kaputa.sk>
2018-10-23 21:18:25 +0200

    try updating the widgets after the background caldav syncing

--- app/src/main/kotlin/com/simplemobiletools/calendar/receivers/CalDAVSyncReceiver.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/receivers/CalDAVSyncReceiver.kt
@@ -4,9 +4,12 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.simplemobiletools.calendar.extensions.recheckCalDAVCalendars
+import com.simplemobiletools.calendar.extensions.updateWidgets
 
 class CalDAVSyncReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        context.recheckCalDAVCalendars {}
+        context.recheckCalDAVCalendars {
+            context.updateWidgets()
+        }
     }
 }
