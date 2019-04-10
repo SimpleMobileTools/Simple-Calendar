e4691d3d90d1359dfef312849336d11425e48361
tibbi <tibor@kaputa.sk>
2019-04-10 10:09:01 +0200

    adding a new FAQ item

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/activities/MainActivity.kt
@@ -781,11 +781,11 @@ class MainActivity : SimpleActivity(), RefreshRecyclerViewListener {
 
         val faqItems = arrayListOf(
                 FAQItem(R.string.faq_1_title_commons, R.string.faq_1_text_commons),
-                FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
                 FAQItem(R.string.faq_4_title_commons, R.string.faq_4_text_commons),
-                FAQItem(getString(R.string.faq_1_title), getString(R.string.faq_1_text)),
-                FAQItem(getString(R.string.faq_2_title), getString(R.string.faq_2_text)),
-                FAQItem(getString(R.string.faq_3_title), getString(R.string.faq_3_text)),
+                FAQItem(R.string.faq_1_title, R.string.faq_1_text),
+                FAQItem(R.string.faq_2_title, R.string.faq_2_text),
+                FAQItem(R.string.faq_3_title, R.string.faq_3_text),
+                FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
                 FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons))
 
         startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
