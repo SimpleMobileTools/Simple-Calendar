393abbaf90b1750ab2db978e8cd5362272525cb6
tibbi <tibor@kaputa.sk>
2019-12-10 22:30:22 +0100

    create an extended list of time zones recognized by joda time

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/helpers/Constants.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/helpers/Constants.kt
@@ -1,7 +1,5 @@
 package com.simplemobiletools.calendar.pro.helpers
 
-import com.simplemobiletools.calendar.pro.models.MyTimeZone
-
 const val LOW_ALPHA = .3f
 const val MEDIUM_ALPHA = .6f
 const val STORED_LOCALLY_ONLY = 0
@@ -161,10 +159,3 @@ const val REMINDER_NOTIFICATION = 0
 const val REMINDER_EMAIL = 1
 
 fun getNowSeconds() = System.currentTimeMillis() / 1000L
-
-// timezones fetched from https://www.joda.org/joda-time/timezones.html
-fun getAllTimeZones() = arrayListOf(
-        MyTimeZone(1, "GMT-11", "Pacific/Midway"),
-        MyTimeZone(2, "GMT-11", "Pacific/Niue"),
-        MyTimeZone(3, "GMT-11", "Pacific/Pago_Pago")
-)
