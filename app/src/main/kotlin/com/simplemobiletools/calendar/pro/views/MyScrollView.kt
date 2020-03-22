6734f60dcd28b06ada28aaf8a382ceca96e20c18
tibbi <tibor@kaputa.sk>
2020-03-22 20:51:24 +0100

    allow making Myscrollview not scrollable

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/views/MyScrollView.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/views/MyScrollView.kt
@@ -2,9 +2,12 @@ package com.simplemobiletools.calendar.pro.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.widget.ScrollView
 
 class MyScrollView : ScrollView {
+    var isScrollable = true
+
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
@@ -20,6 +23,22 @@ class MyScrollView : ScrollView {
     override fun onScrollChanged(x: Int, y: Int, oldx: Int, oldy: Int) {
         super.onScrollChanged(x, y, oldx, oldy)
         scrollViewListener?.onScrollChanged(this, x, y, oldx, oldy)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (isScrollable) {
+            super.onTouchEvent(event)
+        } else {
+            true
+        }
+    }
+
+    override fun onInterceptTouchEvent(event: MotionEvent): Boolean {
+        return if (isScrollable) {
+            super.onInterceptTouchEvent(event)
+        } else {
+            false
+        }
     }
 
     interface ScrollViewListener {
