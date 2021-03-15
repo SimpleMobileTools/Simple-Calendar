18c98171bbc08ed1ff0f48a2bbd5a6bb297cdfc7
yairks <y.k.sachs@gmail.com>
2021-03-15 12:08:52 -0400

    Add option to update this and future occurrences

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/dialogs/EditRepeatingEventDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/dialogs/EditRepeatingEventDialog.kt
@@ -8,13 +8,14 @@ import com.simplemobiletools.commons.extensions.hideKeyboard
 import com.simplemobiletools.commons.extensions.setupDialogStuff
 import kotlinx.android.synthetic.main.dialog_edit_repeating_event.view.*
 
-class EditRepeatingEventDialog(val activity: SimpleActivity, val callback: (allOccurrences: Boolean) -> Unit) {
+class EditRepeatingEventDialog(val activity: SimpleActivity, val callback: (allOccurrences: Int) -> Unit) {
     var dialog: AlertDialog
 
     init {
         val view = (activity.layoutInflater.inflate(R.layout.dialog_edit_repeating_event, null) as ViewGroup).apply {
-            edit_repeating_event_one_only.setOnClickListener { sendResult(false) }
-            edit_repeating_event_all_occurrences.setOnClickListener { sendResult(true) }
+            edit_repeating_event_one_only.setOnClickListener { sendResult(0) }
+            edit_repeating_event_this_and_future_occurences.setOnClickListener { sendResult(1)}
+            edit_repeating_event_all_occurrences.setOnClickListener { sendResult(2) }
         }
 
         dialog = AlertDialog.Builder(activity)
@@ -25,7 +26,7 @@ class EditRepeatingEventDialog(val activity: SimpleActivity, val callback: (allO
                 }
     }
 
-    private fun sendResult(allOccurrences: Boolean) {
+    private fun sendResult(allOccurrences: Int) {
         callback(allOccurrences)
         dialog.dismiss()
     }
