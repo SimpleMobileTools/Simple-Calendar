f9c8b26271446bb69b37aa178edc9ea9b0c65312
tibbi <tibor@kaputa.sk>
2021-04-15 19:14:38 +0200

    reverting a weekly view glitch, it causes more glitches

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/extensions/Range.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/extensions/Range.kt
@@ -2,4 +2,4 @@ package com.simplemobiletools.calendar.pro.extensions
 
 import android.util.Range
 
-fun Range<Int>.touch(other: Range<Int>) = (upper >= other.lower && lower <= other.upper) || (other.upper >= lower && other.lower <= upper)
+fun Range<Int>.touch(other: Range<Int>) = (upper > other.lower && lower < other.upper) || (other.upper > lower && other.lower < upper)
