3db07639bf5264bc7a82c902493497f23cd82c75
Honk2 <shaigan@web.de>
2022-01-11 22:48:36 +0100

    fix issue 1588 overlapp issue by adding crosswise collision checks

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/models/EventWeeklyView.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/models/EventWeeklyView.kt
@@ -2,4 +2,4 @@ package com.simplemobiletools.calendar.pro.models
 
 import android.util.Range
 
-data class EventWeeklyView(val id: Long, val range: Range<Int>)
+data class EventWeeklyView(val range: Range<Int>, var slot: Int = 0, var slot_max: Int = 0, var collisions: ArrayList<Long> = ArrayList())
