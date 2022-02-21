2c74fbe68fc0f201873414fcc63dd19dfa062934
tibbi <tibor@kaputa.sk>
2022-02-21 22:53:16 +0100

    adding some views into Task Activity

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/activities/TaskActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/activities/TaskActivity.kt
@@ -2,8 +2,15 @@ package com.simplemobiletools.calendar.pro.activities
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.WindowManager
 import com.simplemobiletools.calendar.pro.R
+import com.simplemobiletools.calendar.pro.extensions.config
+import com.simplemobiletools.calendar.pro.helpers.TASK_ID
+import com.simplemobiletools.commons.extensions.applyColorFilter
 import com.simplemobiletools.commons.extensions.checkAppSideloading
+import com.simplemobiletools.commons.extensions.updateActionBarTitle
+import com.simplemobiletools.commons.extensions.updateTextColors
+import kotlinx.android.synthetic.main.activity_task.*
 
 class TaskActivity : SimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,10 +20,26 @@ class TaskActivity : SimpleActivity() {
         if (checkAppSideloading()) {
             return
         }
+
+        val intent = intent ?: return
+        val taskId = intent.getLongExtra(TASK_ID, 0L)
+        setupNewTask()
+        updateColors()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         updateMenuItemColors(menu, true)
         return true
+    }
+
+    private fun setupNewTask() {
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        task_title.requestFocus()
+        updateActionBarTitle(getString(R.string.new_task))
+    }
+
+    private fun updateColors() {
+        updateTextColors(task_scrollview)
+        task_time_image.applyColorFilter(config.textColor)
     }
 }
