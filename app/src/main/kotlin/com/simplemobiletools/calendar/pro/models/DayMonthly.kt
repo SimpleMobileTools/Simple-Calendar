d10dd726811158119cd45354a2809c4e4da83d6f
Naveen <snaveen935@gmail.com>
2022-05-18 17:49:19 +0530

    Put parameters on separate lines

--- app/src/main/kotlin/com/simplemobiletools/calendar/pro/models/DayMonthly.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/pro/models/DayMonthly.kt
@@ -1,4 +1,12 @@
 package com.simplemobiletools.calendar.pro.models
 
-data class DayMonthly(val value: Int, val isThisMonth: Boolean, val isToday: Boolean, val code: String, val weekOfYear: Int, var dayEvents: ArrayList<Event>,
-                      var indexOnMonthView: Int, var isWeekend: Boolean)
+data class DayMonthly(
+    val value: Int,
+    val isThisMonth: Boolean,
+    val isToday: Boolean,
+    val code: String,
+    val weekOfYear: Int,
+    var dayEvents: ArrayList<Event>,
+    var indexOnMonthView: Int,
+    var isWeekend: Boolean
+)
